ec52f71038e6508e98f75cb9ec213ac5a107b37d
Main Push Robot <robot@example.com>
2023-03-23 07:27:44 +0000

    Committing dev changes

--- examples/SendSMS/SendSMS.ino
+++ examples/SendSMS/SendSMS.ino
@@ -3,37 +3,31 @@
  *
  * @file        SendSMS.ino
  * @brief       This is a bare bone library for communicating with SIM800
- *    It's barebone in that - it only provides basic functunaitlites while still
+ *    It's barebone in that - it only provides basic functionalities while still
  *    maintaining strong performance and being memory friendly.
  *    It currently supports GSM(sending and reading SMS),
  *    GPRS connectivity(sending and receiving TCP) with Time and Location
  *
- *    This library is writing by  Ayo Ayibiowu.
+ *    This library is written by Ayo Ayibiowu.
  *    charlesayibiowu@hotmail.com
- *    Designed to work with the GSM Sim800l module,
- *    and possibily other SIMxxx modules(not tested with others)
- *
- *    This library use AltSoftSerial, and the pins has already be defined
- *    be sure that GND is attached to arduino too.
- *    The RESET_PIN is not used for this Library, you can leave it un-connected
+ *    Designed to work with the GSM Sim800l module
  *
  *    To Enable Debugging - Go to <BareBoneSim800.h file and change the
  *    #define DEBUG 0 to #define DEBUG 1
  *
  *    PINOUT:
  *        _____________________________
- *       |  ARDUINO UNO >>>   SIM800L  |
+ *       |  Dasduino  >>>   SIM800L   |
  *        -----------------------------
- *            GND      >>>   GND
+ *           GND      >>>   GND
  *        RX  8       >>>   TX
  *        TX  9       >>>   RX
+ *           VCC      >>>   VCC
+ *    Also, you have to connect the PWRKEY pin to the VCC or some digital pin if you want to control the module.
+ *    You can change RX and TX pins callilng setPins() function before begin(), these are the default ones.
  *
  *
- *   POWER SOURCE 4.2V (MAX) >>> VCC
- *
- *
- *
- *   Modified by: soldered.com
+ *   Modified by: soldered.com, 21 March 2023
  *   See more at https://www.solde.red/333071
  *
  * @authors     Created on: Oct 24, 2017
@@ -44,26 +38,33 @@
 
 #include "SIM800L-SOLDERED.h"
 
-BareBoneSim800 sim800; //
-// BareBoneSim800 sim800("your APN");  //needed for gprs funtionality
+SIM800L sim800;
+// SIM800L sim800("your APN");  //needed for gprs funtionality
 
 
 void setup()
 {
-    Serial.begin(115200); //Start serial communication with PC using 115200 baudrate
-    sim800.begin(); //Initialize sim800 module
-    while (!Serial) //Wait until serial is available
+    Serial.begin(115200); // Start serial communication with PC using 115200 baudrate
+    sim800.begin();       // Initialize sim800 module
+    while (!Serial)       // Wait until serial is available
         ;
 
     Serial.println("Testing GSM module For SMS Sending");
     delay(8000); // this delay is necessary, it helps the device to be ready and connect to a network
 
     Serial.println("Should be ready by now");
-    bool deviceAttached = sim800.isAttached(); //Check if sim800 is connected
+    bool deviceAttached = sim800.isAttached(); // Check if sim800 is connected
     if (deviceAttached)
+    {
         Serial.println("Device is Attached");
+    }
     else
+    {
         Serial.println("Not Attached");
+
+        while (1)
+            ;
+    }
 
     // Testing sending SMS aspect
 
